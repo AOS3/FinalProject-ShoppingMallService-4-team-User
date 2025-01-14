5eef64c406efacb828a2f77abe81cc8abae61ec4
bhd47 <bhd4734@gmail.com>
2025-01-14 17:31:55 +0900

    1차 메인화면(home) 완료

--- app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/util/Utils.kt
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/util/Utils.kt
@@ -1,4 +1,21 @@
 package com.aladin.finalproject_shoppingmallservice_4_team.util
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.aladin.finalproject_shoppingmallservice_4_team.R
+
 object Utils {
+}
+
+fun Fragment.replaceMainFragment(fragment: Fragment) {
+    parentFragmentManager.commit {
+        replace(R.id.fragmentContainerView, fragment)
+    }
+}
+
+fun Fragment.replaceSubFragment(fragment: Fragment) {
+    childFragmentManager.commit {
+        replace(R.id.fragmentContainerView_main_sub, fragment)
+    }
 }
