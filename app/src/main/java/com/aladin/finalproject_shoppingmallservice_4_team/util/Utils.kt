4a84d3074e362956aa37014d293a4081b2d8f882
bhd47 <bhd4734@gmail.com>
2025-01-15 14:04:03 +0900

    개발 앱 초기설정

--- app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/util/Utils.kt
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/util/Utils.kt
@@ -6,16 +6,5 @@ import androidx.fragment.app.replace
 import com.aladin.finalproject_shoppingmallservice_4_team.R
 
 object Utils {
-}
 
-fun Fragment.replaceMainFragment(fragment: Fragment) {
-    parentFragmentManager.commit {
-        replace(R.id.fragmentContainerView, fragment)
-    }
-}
-
-fun Fragment.replaceSubFragment(fragment: Fragment) {
-    childFragmentManager.commit {
-        replace(R.id.fragmentContainerView_main_sub, fragment)
-    }
 }
