694e7b55720d88c26e10b884c5e0d5e137cf8279
chash <chash0941@naver.com>
2025-01-15 12:02:46 +0900

    Login Rebase commit

--- app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/main/MainFragment.kt
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/main/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.aladin.finalproject_shoppingmallservice_4_team.R
 import com.aladin.finalproject_shoppingmallservice_4_team.databinding.FragmentMainBinding
+import com.aladin.finalproject_shoppingmallservice_4_team.ui.home.HomeFragment
 import com.aladin.finalproject_shoppingmallservice_4_team.ui.login.LoginFragment
 import com.aladin.finalproject_shoppingmallservice_4_team.util.ShopFragmentName
 import com.aladin.finalproject_shoppingmallservice_4_team.util.replaceMainFragment
