2192a13d31f1d378cc28ecd2772463e12c6fb77f
bhd47 <bhd4734@gmail.com>
2025-01-15 09:06:38 +0900

    1차 최종 메인화면(home) 완료

---
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/model/HomeBookModel.kt
@@ -0,0 +1,8 @@
+package com.aladin.finalproject_shoppingmallservice_4_team.model
+
+data class HomeBookModel (
+    val image: String,
+    val bookName: String,
+    val bookWriter: String,
+    val bookPrice: Int,
+)
--- app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/home/HomeFragment.kt
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/home/HomeFragment.kt
@@ -7,21 +7,113 @@ import android.view.View
 import android.view.ViewGroup
 import com.aladin.finalproject_shoppingmallservice_4_team.R
 import com.aladin.finalproject_shoppingmallservice_4_team.databinding.FragmentHomeBinding
+import com.aladin.finalproject_shoppingmallservice_4_team.util.replaceMainFragment
+import com.google.android.material.tabs.TabLayout
 
 
 class HomeFragment : Fragment() {
 
-    private lateinit var binding: FragmentHomeBinding
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    private val adapter: HomeAdapter by lazy { HomeAdapter() }
+    private var list = MutableList(30) {
+        "항목 $it"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
-
-
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        settingRecyclerView()
+        toolbarButton()
+        tabLayoutButton()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    /*
+    리사이클러 뷰
+     */
+    private fun settingRecyclerView() {
+        binding.recyclerViewHome.adapter = adapter
+        adapter.updateList(list)
+    }
+
+
+    /*
+    툴바 버튼 리스너
+     */
+
+    private fun toolbarButton() {
+        binding.materialToolbarHome.setOnMenuItemClickListener {
+            when(it.itemId) {
+                // 메뉴
+                R.id.home_menu_menuitem -> {
+
+                }
+                // 알림
+                R.id.home_menu_notification -> {
+
+                }
+                // 장바구니
+                R.id.home_menu_shoppingcart -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    /*
+    탭 카테고리 관리
+     */
+
+    private fun tabLayoutButton() {
+        binding.tabLayoutHome.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab?.position) {
+                    // 중고
+                    0 -> {
+                        replaceMainFragment(HomeFragment())
+                    }
+                    // 신작
+                    1 -> {
+                        replaceMainFragment(HomeFragment())
+                    }
+                    // 베스트셀러
+                    2 -> {
+                        replaceMainFragment(HomeFragment())
+                    }
+                    // 국내
+                    3 -> {
+                        replaceMainFragment(HomeFragment())
+                    }
+                    // 해외
+                    4 -> {
+                        replaceMainFragment(HomeFragment())
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
     }
 }
