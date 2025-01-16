4ed30f3617f139ada1e7905f492fd96558579819
bhd47 <bhd4734@gmail.com>
2025-01-16 11:01:35 +0900

    1차 화면 마무리

---
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/orderinquiry/OrderInquiryDetailFragment.kt
@@ -0,0 +1,21 @@
+package com.aladin.finalproject_shoppingmallservice_4_team.ui.orderinquiry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aladin.finalproject_shoppingmallservice_4_team.R
+
+
+class OrderInquiryDetailFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_order_inquiry_detail, container, false)
+    }
+}
