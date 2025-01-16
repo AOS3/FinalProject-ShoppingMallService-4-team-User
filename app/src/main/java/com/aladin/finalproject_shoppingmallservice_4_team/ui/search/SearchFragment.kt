0d6f3adcd5fe8e7348da8b009c3591ea516cab17
bhd47 <bhd4734@gmail.com>
2025-01-16 13:23:09 +0900

    1차 화면 생성

---
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/search/SearchFragment.kt
@@ -0,0 +1,25 @@
+package com.aladin.finalproject_shoppingmallservice_4_team.ui.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aladin.finalproject_shoppingmallservice_4_team.R
+import com.aladin.finalproject_shoppingmallservice_4_team.databinding.FragmentSearchBinding
+
+
+class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
