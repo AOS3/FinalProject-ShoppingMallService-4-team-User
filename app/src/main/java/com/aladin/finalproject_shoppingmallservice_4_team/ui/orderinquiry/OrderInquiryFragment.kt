216bd97f669b6c06056ea27e2c8b8ebe843a7a37
bhd47 <bhd4734@gmail.com>
2025-01-16 16:09:49 +0900

    기능 설정

--- app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/orderinquiry/OrderInquiryFragment.kt
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/orderinquiry/OrderInquiryFragment.kt
@@ -6,15 +6,74 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.aladin.finalproject_shoppingmallservice_4_team.R
+import com.aladin.finalproject_shoppingmallservice_4_team.databinding.FragmentOrderInquiryBinding
+import com.aladin.finalproject_shoppingmallservice_4_team.ui.mainMenu.MainMenuFragment
+import com.aladin.finalproject_shoppingmallservice_4_team.ui.shoppingcart.ShoppingCartFragment
+import com.aladin.finalproject_shoppingmallservice_4_team.util.removeFragment
+import com.aladin.finalproject_shoppingmallservice_4_team.util.replaceMainFragment
+import com.aladin.finalproject_shoppingmallservice_4_team.util.replaceSubFragment
 
 class OrderInquiryFragment : Fragment() {
 
+    private lateinit var binding: FragmentOrderInquiryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order_inquiry, container, false)
+        binding = FragmentOrderInquiryBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        toolbarButton()
+    }
+
+    /*
+    리사이클러 뷰
+     */
+
+    /*
+    툴바
+     */
+
+    private fun toolbarButton() {
+        toolbarBackButton()
+        toolbarMenuButton()
+    }
+
+    private fun toolbarBackButton() = binding.materialToolbarOrderInquiry.setNavigationOnClickListener { removeFragment() }
+
+    private fun toolbarMenuButton() {
+        binding.materialToolbarOrderInquiry.setOnMenuItemClickListener {
+            when(it.itemId) {
+                // 메뉴
+                R.id.home_menu_menuitem -> {
+                    replaceSubFragment(MainMenuFragment(), true)
+                }
+                // 알림
+                R.id.home_menu_notification -> {
+
+                }
+                // 장바구니
+                R.id.home_menu_shoppingcart -> {
+                    replaceMainFragment(ShoppingCartFragment(), true)
+                }
+            }
+            false
+        }
+    }
+
+    /*
+    버튼
+     */
+
+    private fun settingAskButton() {
+        binding.buttonOrderInquiryAsk.setOnClickListener {
+            // 문의 화면으로 변경한다.
+        }
     }
 }
