b54f93b02d05c6b75f43cad9c0064d5a28008a38
cys020628 <cys020628@naver.com>
2025-01-16 15:41:48 +0900

    툴바 설정

--- app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/notice/NoticeFragment.kt
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/notice/NoticeFragment.kt
@@ -7,16 +7,28 @@ import android.view.View
 import android.view.ViewGroup
 import com.aladin.finalproject_shoppingmallservice_4_team.R
 import com.aladin.finalproject_shoppingmallservice_4_team.databinding.FragmentNoticeBinding
+import com.aladin.finalproject_shoppingmallservice_4_team.ui.mainMenu.MainMenuFragment
+import com.aladin.finalproject_shoppingmallservice_4_team.util.removeFragment
+import com.aladin.finalproject_shoppingmallservice_4_team.util.replaceSubFragment
 
 
 class NoticeFragment : Fragment() {
-  private lateinit var fragmentNoticeBinding: FragmentNoticeBinding
+    private lateinit var fragmentNoticeBinding: FragmentNoticeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        fragmentNoticeBinding = FragmentNoticeBinding.inflate(layoutInflater,container,false)
+        fragmentNoticeBinding = FragmentNoticeBinding.inflate(layoutInflater, container, false)
+        // 툴바 설정
+        settingToolbar()
         return fragmentNoticeBinding.root
     }
 
+    private fun settingToolbar() {
+        fragmentNoticeBinding.apply {
+            materialToolbarChangePw.setNavigationOnClickListener {
+                removeFragment()
+            }
+        }
+    }
 }
