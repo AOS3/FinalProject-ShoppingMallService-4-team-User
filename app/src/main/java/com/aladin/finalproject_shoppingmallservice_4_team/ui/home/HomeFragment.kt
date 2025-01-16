12ab851336320edf1a3fa080a1c75e75efce3eef
bhd47 <bhd4734@gmail.com>
2025-01-16 13:44:00 +0900

    버튼들 누르면 화면 이동하도록 구성

--- app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/home/HomeFragment.kt
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/home/HomeFragment.kt
@@ -8,7 +8,9 @@ import android.view.ViewGroup
 import com.aladin.finalproject_shoppingmallservice_4_team.R
 import com.aladin.finalproject_shoppingmallservice_4_team.databinding.FragmentHomeBinding
 import com.aladin.finalproject_shoppingmallservice_4_team.ui.bookdetail.BookDetailFragment
+import com.aladin.finalproject_shoppingmallservice_4_team.ui.booklist.BookListFragment
 import com.aladin.finalproject_shoppingmallservice_4_team.ui.mainMenu.MainMenuFragment
+import com.aladin.finalproject_shoppingmallservice_4_team.ui.shoppingcart.ShoppingCartFragment
 import com.aladin.finalproject_shoppingmallservice_4_team.util.replaceMainFragment
 import com.aladin.finalproject_shoppingmallservice_4_team.util.replaceSubFragment
 import com.google.android.material.tabs.TabLayout
@@ -38,6 +40,7 @@ class HomeFragment : Fragment(), HomeOnClickListener {
         settingRecyclerView()
         toolbarButton()
         tabLayoutButton()
+        settingSearchButton()
     }
 
     override fun onDestroyView() {
@@ -48,9 +51,16 @@ class HomeFragment : Fragment(), HomeOnClickListener {
     /*
     리사이클러 뷰
      */
+
     private fun settingRecyclerView() {
+        // 리사이클러 뷰 구성
         binding.recyclerViewHome.adapter = adapter
         adapter.updateList(list)
+    }
+
+    override fun itemClickListener(position: Int) {
+        // 상세 화면으로 이동한다
+        replaceMainFragment(BookDetailFragment(), true)
     }
 
 
@@ -71,7 +81,7 @@ class HomeFragment : Fragment(), HomeOnClickListener {
                 }
                 // 장바구니
                 R.id.home_menu_shoppingcart -> {
-
+                    replaceMainFragment(ShoppingCartFragment(), true)
                 }
             }
             true
@@ -88,23 +98,23 @@ class HomeFragment : Fragment(), HomeOnClickListener {
                 when(tab?.position) {
                     // 중고
                     0 -> {
-                        replaceMainFragment(HomeFragment(), false)
+                        replaceSubFragment(BookListFragment(), true)
                     }
                     // 신작
                     1 -> {
-                        replaceMainFragment(HomeFragment(), false)
+                        replaceSubFragment(BookListFragment(), true)
                     }
                     // 베스트셀러
                     2 -> {
-                        replaceMainFragment(HomeFragment(), false)
+                        replaceSubFragment(BookListFragment(), true)
                     }
                     // 국내
                     3 -> {
-                        replaceMainFragment(HomeFragment(), false)
+                        replaceSubFragment(BookListFragment(), true)
                     }
                     // 해외
                     4 -> {
-                        replaceMainFragment(HomeFragment(), false)
+                        replaceSubFragment(BookListFragment(), true)
                     }
                 }
             }
@@ -120,7 +130,19 @@ class HomeFragment : Fragment(), HomeOnClickListener {
         })
     }
 
-    override fun itemClickListener(position: Int) {
-        replaceMainFragment(BookDetailFragment(), true)
+    /*
+    버튼
+     */
+
+    private fun settingSearchButton() {
+        binding.buttonHomeSearch.setOnClickListener {
+            // 검색 화면으로 이동한다.
+        }
+    }
+
+    private fun settingMoreButton() {
+        binding.buttonHomeMore.setOnClickListener {
+            replaceSubFragment(BookListFragment(), true)
+        }
     }
 }
