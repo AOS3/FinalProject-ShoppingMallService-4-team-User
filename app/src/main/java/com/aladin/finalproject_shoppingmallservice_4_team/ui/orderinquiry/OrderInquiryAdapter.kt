da29837764343595a379e0fb820ab37cf476e05b
bhd47 <bhd4734@gmail.com>
2025-01-16 16:33:49 +0900

    기능 설정

--- app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/orderinquiry/OrderInquiryAdapter.kt
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/orderinquiry/OrderInquiryAdapter.kt
@@ -1,4 +1,27 @@
 package com.aladin.finalproject_shoppingmallservice_4_team.ui.orderinquiry
 
-class OrderInquiryAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.aladin.finalproject_shoppingmallservice_4_team.databinding.OrderInquiryListItemBinding
+
+class OrderInquiryAdapter: RecyclerView.Adapter<OrderInquiryAdapter.OrderInquiryViewHolder>() {
+
+    private val items = mutableListOf<String>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderInquiryViewHolder {
+        return OrderInquiryViewHolder(OrderInquiryListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: OrderInquiryViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    class OrderInquiryViewHolder(private val binding: OrderInquiryListItemBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: String) {
+
+        }
+    }
 }
