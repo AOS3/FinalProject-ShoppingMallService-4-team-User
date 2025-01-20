bf1d867d0f439666859be04de4416ccbd06457ff
cys020628 <cys020628@naver.com>
2025-01-20 14:28:50 +0900

    이용약관 기능 구현 완료ㅕ

--- app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/guide/GuideFragment.kt
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/guide/GuideFragment.kt
@@ -5,14 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.aladin.finalproject_shoppingmallservice_4_team.R
 import com.aladin.finalproject_shoppingmallservice_4_team.databinding.FragmentGuideBinding
+import com.aladin.finalproject_shoppingmallservice_4_team.ui.ask.AskFragment
 import com.aladin.finalproject_shoppingmallservice_4_team.util.removeFragment
+import com.aladin.finalproject_shoppingmallservice_4_team.util.replaceSubFragment
 
 
 class GuideFragment : Fragment() {
     private lateinit var fragmentGuideBinding: FragmentGuideBinding
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,13 +21,25 @@ class GuideFragment : Fragment() {
         fragmentGuideBinding = FragmentGuideBinding.inflate(layoutInflater,container,false)
         // 툴바 설정
         settingToolbar()
+        // Footer 설정
+        settingFooter()
         return fragmentGuideBinding.root
     }
 
+    // Toolbar
     private fun settingToolbar() {
         fragmentGuideBinding.apply {
             materialToolbarGuide.setNavigationOnClickListener {
                 removeFragment()
+            }
+        }
+    }
+
+    // Footer
+    private fun settingFooter() {
+        fragmentGuideBinding.apply {
+            buttonGuideAsk.setOnClickListener {
+                replaceSubFragment(AskFragment(),true)
             }
         }
     }
