d9ae7a1b12c4872b04abd432f09162de3320e222
cys020628 <cys020628@naver.com>
2025-01-21 16:35:17 +0900

    삭제 기능, 체크박스 기능 검새화면 기능 추가 완료 체크 박스 후에 리팩토리 예정 구매하기 버튼 기능 구현 필요 회원에 따라 분기처리 필요

--- app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/shoppingcart/ShoppingCartFragment.kt
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/shoppingcart/ShoppingCartFragment.kt
@@ -1,46 +1,232 @@
 package com.aladin.finalproject_shoppingmallservice_4_team.ui.shoppingcart
 
 import android.os.Bundle
+import android.util.TypedValue
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.aladin.finalproject_shoppingmallservice_4_team.R
 import com.aladin.finalproject_shoppingmallservice_4_team.databinding.FragmentShoppingCartBinding
+import com.aladin.finalproject_shoppingmallservice_4_team.model.BookListModel
+import com.aladin.finalproject_shoppingmallservice_4_team.model.ShoppingCartModel
+import com.aladin.finalproject_shoppingmallservice_4_team.ui.adapter.NewBookListAdapter
+import com.aladin.finalproject_shoppingmallservice_4_team.ui.adapter.ShoppingCartAdapter
+import com.aladin.finalproject_shoppingmallservice_4_team.ui.adapter.UsedBookListAdapter
+import com.aladin.finalproject_shoppingmallservice_4_team.ui.custom.CustomDialog
+import com.aladin.finalproject_shoppingmallservice_4_team.ui.custom.CustomDialogProgressbar
 import com.aladin.finalproject_shoppingmallservice_4_team.ui.mainMenu.MainMenuFragment
 import com.aladin.finalproject_shoppingmallservice_4_team.ui.notice.NoticeFragment
+import com.aladin.finalproject_shoppingmallservice_4_team.ui.search.SearchFragment
 import com.aladin.finalproject_shoppingmallservice_4_team.util.removeFragment
 import com.aladin.finalproject_shoppingmallservice_4_team.util.replaceMainFragment
+import com.aladin.finalproject_shoppingmallservice_4_team.util.replaceSubFragment
+import com.aladin.finalproject_shoppingmallservice_4_team.util.toCommaString
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ShoppingCartFragment : Fragment() {
-
     private lateinit var fragmentShoppingCartBinding: FragmentShoppingCartBinding
+    private lateinit var shoppingCartAdapter: ShoppingCartAdapter
+    private val shoppingCartViewModel: ShoppingCartViewModel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        fragmentShoppingCartBinding = FragmentShoppingCartBinding.inflate(layoutInflater,container,false)
+        fragmentShoppingCartBinding =
+            FragmentShoppingCartBinding.inflate(layoutInflater, container, false)
+
+        // 쇼핑카트 데이터 가져오기
+        shoppingCartViewModel.gettingShoppingCartBookData()
+
         // 툴바 세팅
         settingToolbar()
+
+        // 다이얼로그
+        observeProgressDialog()
+
+        // 리싸이클러뷰
+        setupRecyclerView()
+
+        // 옵저버
+        observeBookListData()
+
+        // 체크박스
+        settingCheckBox()
+
+        // 검색창 클릭 이벤트
+        onClickSearchButton()
+
+        // 전체 삭제 클릭 이벤트
+        onClickDeleteAllButton()
+
         return fragmentShoppingCartBinding.root
     }
 
+    private fun onClickDeleteAllButton() {
+        fragmentShoppingCartBinding.apply {
+            buttonShoppingCartDeleteList.setOnClickListener {
+                val twoButtonDialog = CustomDialog(
+                    requireContext(),
+                    onPositiveClick = {
+                        val deleteList = shoppingCartAdapter.getCheckedItemsWithUserTokenAndIsbn()
+                        if (deleteList.isEmpty()) {
+                            Toast.makeText(
+                                requireContext(),
+                                "현재 체크된 리스트가 없습니다.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            // 리스트 삭제
+                            shoppingCartViewModel.deleteCheckedShoppingCartBookList(deleteList)
+                            // 리스트 업데이트
+                            shoppingCartAdapter.updateShoppingCartDataList(shoppingCartViewModel.shoppingCartBookList.value!!)
+                        }
+                    },
+                    onNegativeClick = {
+
+                    },
+                    positiveText = "삭제",
+                    negativeText = "취소",
+                    contentText = "체크한 장바구니 리스트를 전부 삭제하시겠습니까?",
+                    icon = R.drawable.error_24px
+                )
+                twoButtonDialog.showCustomDialog()
+            }
+        }
+    }
+
+    // onClickSearch
+    private fun onClickSearchButton() {
+        fragmentShoppingCartBinding.apply {
+            buttonShoppingCartSearch.setOnClickListener {
+                replaceMainFragment(SearchFragment(), true)
+            }
+        }
+    }
+
+    // CheckBox
+    private fun settingCheckBox() {
+        fragmentShoppingCartBinding.apply {
+            checkBoxShoppingCartCheckAllList.setOnCheckedChangeListener { _, isChecked ->
+                // 모든 항목 선택/해제
+                shoppingCartAdapter.selectAllItems(isChecked)
+                calculateTotals()
+            }
+        }
+    }
+
+    // 총 구매 가격과 총 수량 계산
+    private fun calculateTotals() {
+        val selectedItems = shoppingCartAdapter.getSelectedItems()
+        val totalSize = selectedItems.sumOf { it.shoppingCartBookQualityCount }
+        val totalPrice = selectedItems.sumOf { it.shoppingCartSellingPrice * it.shoppingCartBookQualityCount }
+
+        fragmentShoppingCartBinding.apply {
+            recyclerViewShoppingCartTotalListSize.text = "총수량 : ${totalSize}개"
+            recyclerViewShoppingCartTotalListPrice.text = "총 구매 가격 : ${totalPrice.toCommaString()}원"
+        }
+    }
+
+    // Toolbar
     private fun settingToolbar() {
         fragmentShoppingCartBinding.apply {
             materialToolbarShoppingCart.setNavigationOnClickListener {
                 removeFragment()
             }
             materialToolbarShoppingCart.setOnMenuItemClickListener {
-                when(it.itemId) {
+                when (it.itemId) {
                     R.id.item_shoppingCart_notification -> {
-                        replaceMainFragment(NoticeFragment(),true)
+                        replaceMainFragment(NoticeFragment(), true)
                         true
                     }
+
                     else -> {
-                        replaceMainFragment(MainMenuFragment(),true)
+                        replaceMainFragment(MainMenuFragment(), true)
                         true
                     }
+                }
+            }
+        }
+    }
+
+    // Dialog
+    private fun observeProgressDialog() {
+        // 화면 입장 시 공지사항 로딩을 위한 Dialog
+        val progressBarDialog = CustomDialogProgressbar(requireContext())
+        progressBarDialog.show()
+
+        shoppingCartViewModel.isLoadShoppingCartList.observe(viewLifecycleOwner) {
+            if (it) {
+                progressBarDialog.dismiss()
+            }
+        }
+    }
+
+    // RecyclerView
+    private fun setupRecyclerView() {
+        fragmentShoppingCartBinding.apply {
+            shoppingCartAdapter =
+                ShoppingCartAdapter(
+                    emptyList(),
+                    this@ShoppingCartFragment,
+                    shoppingCartViewModel
+                ) { calculateTotals() } // 초기값으로 빈 리스트
+            recyclerViewShoppingCartShoppingCartList.run {
+                layoutManager = LinearLayoutManager(context)
+                adapter = shoppingCartAdapter
+            }
+
+        }
+    }
+
+    // Observer
+    private fun observeBookListData() {
+        fragmentShoppingCartBinding.apply {
+            // LiveData에서 데이터를 받아와서 RecyclerView 업데이트
+            shoppingCartViewModel.shoppingCartBookList.observe(viewLifecycleOwner) { usedBookList ->
+                if (usedBookList.isNotEmpty()) {
+                    // 데이터를 받아왔을 때, RecyclerView에 전달
+                    shoppingCartAdapter =
+                        ShoppingCartAdapter(
+                            usedBookList,
+                            this@ShoppingCartFragment,
+                            shoppingCartViewModel
+                        ) { calculateTotals() } // 새 데이터로 Adapter를 갱신
+                    recyclerViewShoppingCartShoppingCartList.adapter = shoppingCartAdapter
+                    linearLayoutShoppingCartEmptyShoppingCartList.visibility = View.GONE
+                    recyclerViewShoppingCartTotalListSize.visibility = View.VISIBLE
+                    recyclerViewShoppingCartTotalListPrice.visibility = View.VISIBLE
+                    buttonShoppingCartDeleteList.visibility = View.VISIBLE
+                    linearLayoutShoppingCartSelectAll.visibility = View.VISIBLE
+                    linearLayoutShoppingCartSelectAll.visibility = View.VISIBLE
+                    textViewShoppingCartListInfo.visibility = View.VISIBLE
+                } else {
+                    // 데이터가 없을 시
+                    linearLayoutShoppingCartEmptyShoppingCartList.visibility = View.VISIBLE
+                    recyclerViewShoppingCartTotalListSize.visibility = View.GONE
+                    recyclerViewShoppingCartTotalListPrice.visibility = View.GONE
+                    buttonShoppingCartDeleteList.visibility = View.GONE
+                    recyclerViewShoppingCartShoppingCartList.width
+                    linearLayoutShoppingCartSelectAll.visibility = View.INVISIBLE
+                    textViewShoppingCartListInfo.visibility = View.INVISIBLE
+
+                    // 데이터가 없을 시 최소 heigt 설정
+                    val heightInPx = TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP,
+                        300f, // 300dp
+                        resources.displayMetrics
+                    ).toInt()
+
+                    // RecyclerView에 접근하고 너비를 설정
+                    recyclerViewShoppingCartShoppingCartList.layoutParams.height = heightInPx
                 }
             }
         }
--- app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/shoppingcart/ShoppingCartViewModel.kt
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/shoppingcart/ShoppingCartViewModel.kt
@@ -1,6 +1,110 @@
 package com.aladin.finalproject_shoppingmallservice_4_team.ui.shoppingcart
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aladin.finalproject_shoppingmallservice_4_team.model.ShoppingCartModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class ShoppingCartViewModel:ViewModel() {
+@HiltViewModel
+class ShoppingCartViewModel @Inject constructor(private val shoppingCartRepository: ShoppingCartRepository) :
+    ViewModel() {
+
+    // 장바구니 리스트
+    private val _shoppingCartBookList = MutableLiveData<List<ShoppingCartModel>>()
+    val shoppingCartBookList: LiveData<List<ShoppingCartModel>> get() = _shoppingCartBookList
+
+    // 장바구니 리스트를 가져왔는지
+    private val _isLoadShoppingCartList = MutableLiveData<Boolean>(false)
+    val isLoadShoppingCartList: LiveData<Boolean> get() = _isLoadShoppingCartList
+
+    fun testasd() {
+        val shoppingCartItems = listOf(
+            ShoppingCartModel(
+                shoppingCartBookQualityCount = 2,
+                shoppingCartISBN = "9791197613005",
+                shoppingCartQuality = 0,
+                shoppingCartSellingPrice = 30000,
+                shoppingCartState = 0,
+                shoppingCartTime = 1231233213L,
+                shoppingCartUserToken = "test"
+            ),
+            ShoppingCartModel(
+                shoppingCartBookQualityCount = 1,
+                shoppingCartISBN = "9788962626421",
+                shoppingCartQuality = 1,
+                shoppingCartSellingPrice = 20000,
+                shoppingCartState = 1,
+                shoppingCartTime = 1631233213L,
+                shoppingCartUserToken = "exampleUser"
+            )
+        )
+        // Firestore에 데이터 추가
+        viewModelScope.launch {
+            val result = shoppingCartRepository.createShoppingCartBookData(shoppingCartItems)
+            if (result) {
+                println("데이터가 성공적으로 생성되었습니다!")
+            } else {
+                println("데이터 생성에 실패했습니다.")
+            }
+        }
+    }
+
+    // 책 데이터 가져오기
+    fun gettingShoppingCartBookData() {
+        viewModelScope.launch {
+            val (shoppingCartTableList, bookDataList) = withContext(Dispatchers.IO) {
+                shoppingCartRepository.gettingBookDataFromFireBaseStore()
+            }
+
+            // 가져온 리스트들 Model에 업데이트
+            for (i in shoppingCartTableList.indices) {
+                // getOrNull로 가져온 값이 null일 경우 안전하게 처리
+                shoppingCartTableList[i].shoppingCartBookCoverImage =
+                    bookDataList.first.getOrNull(i) ?: "" // 기본값을 설정
+                shoppingCartTableList[i].shoppingCartBookTitle =
+                    bookDataList.second.getOrNull(i) ?: "Unknown Title" // 기본값 설정
+                shoppingCartTableList[i].shoppingCartBookAuthor =
+                    bookDataList.third.getOrNull(i) ?: "Unknown Author" // 기본값 설정
+            }
+
+            // 업데이트된 리스트 전달
+            _shoppingCartBookList.postValue(shoppingCartTableList)
+            Log.e("asd", shoppingCartTableList.toString())
+
+            // 가져온 리스트가 비어있지 않다면
+            if (shoppingCartTableList.isNotEmpty()) {
+                _isLoadShoppingCartList.postValue(true)
+            } else {
+                _isLoadShoppingCartList.postValue(true)
+            }
+        }
+    }
+
+    // 체크된 장바구니 데이터 삭제
+    fun deleteCheckedShoppingCartBookList(deleteList: List<Pair<String, String>>) {
+        viewModelScope.launch {
+            val currentList = _shoppingCartBookList.value.orEmpty()
+
+            // IO 스레드에서 삭제 작업 수행
+            val updatedList = withContext(Dispatchers.IO) {
+                // Firestore에서 데이터 삭제
+                shoppingCartRepository.deleteShoppingCartBookData(deleteList)
+
+                // 삭제된 데이터를 현재 리스트에서 제외
+                currentList.filterNot { item ->
+                    deleteList.any { it.first == item.shoppingCartUserToken && it.second == item.shoppingCartISBN }
+                }
+            }
+            // LiveData에 갱신된 리스트 반영
+            _shoppingCartBookList.value = updatedList
+        }
+    }
 }
+
