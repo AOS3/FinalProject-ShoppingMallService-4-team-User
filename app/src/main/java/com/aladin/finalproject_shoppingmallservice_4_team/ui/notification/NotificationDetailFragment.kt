058386ce17791c99f26b230ab33461e7ce1c4977
bhd47 <bhd4734@gmail.com>
2025-01-21 16:59:15 +0900

    binding 수정

--- app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/notification/NotificationDetailFragment.kt
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/notification/NotificationDetailFragment.kt
@@ -12,15 +12,25 @@ import com.aladin.finalproject_shoppingmallservice_4_team.databinding.FragmentNo
 
 class NotificationDetailFragment : Fragment() {
 
-    private lateinit var binding: FragmentNotificationDetailBinding
+    private var _binding: FragmentNotificationDetailBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentNotificationDetailBinding.inflate(inflater, container, false)
+        _binding = FragmentNotificationDetailBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
