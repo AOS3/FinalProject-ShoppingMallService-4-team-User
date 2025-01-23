38002d3efb765c26a53fac8507281b2368952264
cys020628 <cys020628@naver.com>
2025-01-23 10:49:28 +0900

    settingFragment replace Sub로 변경

--- app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/setting/SettingFragment.kt
@@ -9,16 +9,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat.startActivity
 import com.aladin.finalproject_shoppingmallservice_4_team.MainActivity
-import com.aladin.finalproject_shoppingmallservice_4_team.R
 import com.aladin.finalproject_shoppingmallservice_4_team.databinding.FragmentSettingBinding
 import com.aladin.finalproject_shoppingmallservice_4_team.ui.changePw.ChangePwFragment
 import com.aladin.finalproject_shoppingmallservice_4_team.ui.guide.GuideFragment
-import com.aladin.finalproject_shoppingmallservice_4_team.ui.main.MainFragment
 import com.aladin.finalproject_shoppingmallservice_4_team.ui.notice.NoticeFragment
 import com.aladin.finalproject_shoppingmallservice_4_team.util.removeFragment
-import com.aladin.finalproject_shoppingmallservice_4_team.util.replaceMainFragment
+import com.aladin.finalproject_shoppingmallservice_4_team.util.replaceSubFragment
 
 
 class SettingFragment : Fragment() {
@@ -50,15 +47,15 @@ class SettingFragment : Fragment() {
         fragmentSettingBinding.apply {
             // 공지사항
             viewSettingNotice.setOnClickListener {
-                replaceMainFragment(NoticeFragment(), true)
+                replaceSubFragment(NoticeFragment(), true)
             }
             // 이용 약관
             viewSettingGuide.setOnClickListener {
-                replaceMainFragment(GuideFragment(), true)
+                replaceSubFragment(GuideFragment(), true)
             }
             // 비밀번호 변경
             viewSettingChangePw.setOnClickListener {
-                replaceMainFragment(ChangePwFragment(), true)
+                replaceSubFragment(ChangePwFragment(), true)
             }
             // 카메라 권한 설정
             viewSettingCameraPermission.setOnClickListener {
