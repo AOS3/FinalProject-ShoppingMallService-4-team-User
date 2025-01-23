bc0f4ca63e3f832a08c9ad1b9c5af4cd3d22ace7
KSJ <plplplplo48@gmail.com>
2025-01-23 18:56:57 +0900

    SellingCart 버튼 설정 및 api, firebase 연결

---
+++ app/src/main/java/com/aladin/finalproject_shoppingmallservice_4_team/ui/sellingcart/SellingCartRepository.kt
@@ -0,0 +1,18 @@
+package com.aladin.finalproject_shoppingmallservice_4_team.ui.sellingcart
+
+import com.aladin.apiTestApplication.dto.BookItem
+import com.aladin.apiTestApplication.network.AladdinApiService
+import com.aladin.finalproject_shoppingmallservice_4_team.BuildConfig
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SellingCartRepository @Inject constructor(
+    private val apiService: AladdinApiService
+) {
+    // 책 검색 API 호출 함수
+    suspend fun searchBooks(query: String, maxResults: Int, sort: String): List<BookItem> {
+        val response = apiService.searchBooks(BuildConfig.API_KEY, query, maxResults = maxResults, sort = sort)
+        return response.items
+    }
+}
